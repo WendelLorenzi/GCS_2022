230f6efd7637697e9e8b4d0021f75a72df6e2bbe
WendelLorenzi <wlorenzi17@gmail.com>
2022-05-12 00:00:56 -0300

    feat: developed class serialC

---
+++ GUI_2022/src/serial/Path_txt.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+import enum
+
+
+class Txt(enum.Enum):
+    ALTITUDEC = "/home/wendel/Área de trabalho/LtSat/GCS_2022/GUI_2022/datasets/txtTransicao/altitudeC.txt"
+    TEMPERATURAC = '/home/wendel/Área de trabalho/LtSat/GCS_2022/GUI_2022/datasets/txtTransicao/temperaturaC.txt'
+    VOLTAGEC= '/home/wendel/Área de trabalho/LtSat/GCS_2022/GUI_2022/datasets/txtTransicao/VoltageC.txt'
+    GPSLATITUDE= '/home/wendel/Área de trabalho/LtSat/GCS_2022/GUI_2022/datasets/txtTransicao/gpsLatitudeC.txt'
+    GPSLONGITUDE= '/home/wendel/Área de trabalho/LtSat/GCS_2022/GUI_2022/datasets/txtTransicao/gpsLongitudeC.txt'
+    GPSALTURAC= '/home/wendel/Área de trabalho/LtSat/GCS_2022/GUI_2022/datasets/txtTransicao/GpsAlturaC.txt'
+    ALTITUDEP= '/home/wendel/Área de trabalho/LtSat/GCS_2022/GUI_2022/datasets/txtTransicao/AltitudeP.txt'
+    TEMPERATURAP= '/home/wendel/Área de trabalho/LtSat/GCS_2022/GUI_2022/datasets/txtTransicao/TemperaturaP.txt'
+    VOLTAGEP= '/home/wendel/Área de trabalho/LtSat/GCS_2022/GUI_2022/datasets/txtTransicao/VoltageP.txt'
+    GIROSCOPIOR= '/home/wendel/Área de trabalho/LtSat/GCS_2022/GUI_2022/datasets/txtTransicao/GiroscopioR.txt'
+    GIROSCOPIOP= '/home/wendel/Área de trabalho/LtSat/GCS_2022/GUI_2022/datasets/txtTransicao/GiroscopioP.txt'
+    GIROSCOPIOY= '/home/wendel/Área de trabalho/LtSat/GCS_2022/GUI_2022/datasets/txtTransicao/GiroscopioY.txt'
+    ACELEROMETROR= '/home/wendel/Área de trabalho/LtSat/GCS_2022/GUI_2022/datasets/txtTransicao/AcelerometroR.txt'
+    ACELEROMETROP= '/home/wendel/Área de trabalho/LtSat/GCS_2022/GUI_2022/datasets/txtTransicao/AcelerometroP.txt'
+    ACELEROMETROY= '/home/wendel/Área de trabalho/LtSat/GCS_2022/GUI_2022/datasets/txtTransicao/AcelerometroY.txt'
+    MAGNETOMETROR= '/home/wendel/Área de trabalho/LtSat/GCS_2022/GUI_2022/datasets/txtTransicao/MagnetometroR.txt'
+    MAGNETOMETROP= '/home/wendel/Área de trabalho/LtSat/GCS_2022/GUI_2022/datasets/txtTransicao/MagnetometroP.txt'
+    MAGNETOMETROY= '/home/wendel/Área de trabalho/LtSat/GCS_2022/GUI_2022/datasets/txtTransicao/MagnetometroY.txt'
--- GUI_2022/src/serial/index.py
+++ GUI_2022/src/serial/index.py
@@ -1,55 +1,98 @@
 # -*- coding: utf-8 -*-
 import serial
-import time
-import pandas as pd
-from utils import Path_txt
+from Path_txt import Txt
 from conection import ConectionS
 
 
-class serial:
-    #MSP = serial.Serial('/dev/ttyACM0', 9600, timeout=3)
-    
-    def Captura(self, MSP):
-        arquivoAltitudeC = open(Path_txt.ALTITUDEC, 'w')
-        arquivoTemperaturaC = open(Path_txt.TEMPERATURAC, 'w')
-        arquivoVoltageC= open(Path_txt.VOLTAGEC, 'w')
-        try:
-            while MSP.bytesize > 0:
-                MSP.flushOutput()
-                MSP.flushInput()
+class serialC:  
+        def Captura(self, ConectionS):
+            MSP = serial.Serial('/dev/ttyACM0', 9600, timeout=3)
+            arquivoAltitudeC = open('/home/wendel/Área de trabalho/LtSat/GCS_2022/GUI_2022/datasets/txtTransicao/altitudeC.txt', 'w')
+            arquivoTemperaturaC = open(str(Txt.TEMPERATURAC), 'w')
+            arquivoVoltageC= open(str(Txt.VOLTAGEC), 'w')
+            arquivoGpsLatitudeC= open(str(Txt.GPSLATITUDE), 'w')
+            arquivoGpsLongitudeC= open(str(Txt.GPSLONGITUDE), 'w')
+            arquivoGpsAlturaC= open(str(Txt.GPSALTURAC), 'w')
+            arquivoAltitudeP= open(str(Txt.ALTITUDEP), 'w')
+            arquivoTemperaturaP= open(str(Txt.TEMPERATURAP), 'w')
+            arquivosetVoltageP= open(str(Txt.VOLTAGEP), 'w')
+            arquivoGiroscopioR= open(str(Txt.GIROSCOPIOR), 'w')
+            arquivoGiroscopioP= open(str(Txt.GIROSCOPIOP), 'w')
+            arquivoGiroscopioY= open(str(Txt.GIROSCOPIOY), 'w')
+            arquivoAcelerometroR= open(str(Txt.ACELEROMETROR), 'w')
+            arquivoAcelerometroP= open(str(Txt.ACELEROMETROP), 'w')
+            arquivoAcelerometroY= open(str(Txt.ACELEROMETROY),'w')
+            arquivoMagnetometroR= open(str(Txt.MAGNETOMETROR),'w')
+            arquivoMagnetometroP= open(str(Txt.MAGNETOMETROP),'w')
+            arquivoMagnetometroY= open(str(Txt.MAGNETOMETROY),'w')
+            try:
+                while MSP.bytesize > 0:
+                    MSP.flushOutput()
+                    MSP.flushInput()
 
-                data = MSP.readline().decode()
-                dataSplit= data.split(',')
-                print(dataSplit)
-                if len(dataSplit) > 1:
-                    #filterVector(dataSplit)
-                    if dataSplit[3] == "c":
-                        ConectionS.setAltitudeC(dataSplit[0])
-                        ConectionS.setTemperaturaC(dataSplit[1])
-                        ConectionS.setVoltageC(dataSplit[2])
-                        ConectionS.setGpsLatitudeC(dataSplit[4])
-                        ConectionS.setGpsLongitudeC(dataSplit[5])
-                        ConectionS.setGpsAlturaC(dataSplit[6])
-                    elif dataSplit[3] == "t":
-                        ConectionS.setAltitudeP(dataSplit[7])
-                        ConectionS.setTemperaturaP(dataSplit[8])
-                        ConectionS.setVoltageP(dataSplit[9])
-                        ConectionS.setGiroscopioR(dataSplit[10])
-                        ConectionS.setGiroscopioP(dataSplit[11])
-                        ConectionS.setGiroscopioY(dataSplit[12])
-                        ConectionS.setAcelerometroR(dataSplit[13])
-                        ConectionS.setAcelerometroP(dataSplit[14])
-                        ConectionS.setAcelerometroY(dataSplit[15])
-                        ConectionS.setMagnetometroR(dataSplit[16])
-                        ConectionS.setMagnetometroP(dataSplit[17])
-                        ConectionS.setMagnetometroY(dataSplit[18])
-        finally:
-            MSP.close()
-            arquivoAltitudeC.write(str(ConectionS.getAltitudeC()))
-            arquivoTemperaturaC.write(str(ConectionS.getTemperaturaC()))
-            arquivoVoltageC.write(str(ConectionS.getVoltageC()))
-            arquivoAltitudeC.close()
-            arquivoTemperaturaC.close()
+                    data = MSP.readline().decode()
+                    dataSplit= data.split(',')
+                    print(dataSplit)
+                    if len(dataSplit) > 1:
+                        #filterVector(dataSplit)
+                        if (dataSplit[3] == "c"):
+                            ConectionS.setAltitudeC(dataSplit[1])
+                            ConectionS.setTemperaturaC(dataSplit[1])
+                            ConectionS.setVoltageC(dataSplit[2])
+                            ConectionS.setGpsLatitudeC(dataSplit[4])
+                            ConectionS.setGpsLongitudeC(dataSplit[5])
+                            ConectionS.setGpsAlturaC(dataSplit[6])
+                        elif dataSplit[3] == "t":
+                            ConectionS.setAltitudeP(dataSplit[7])
+                            ConectionS.setTemperaturaP(dataSplit[8])
+                            ConectionS.setVoltageP(dataSplit[9])
+                            ConectionS.setGiroscopioR(dataSplit[10])
+                            ConectionS.setGiroscopioP(dataSplit[11])
+                            ConectionS.setGiroscopioY(dataSplit[12])
+                            ConectionS.setAcelerometroR(dataSplit[13])
+                            ConectionS.setAcelerometroP(dataSplit[14])
+                            ConectionS.setAcelerometroY(dataSplit[15])
+                            ConectionS.setMagnetometroR(dataSplit[16])
+                            ConectionS.setMagnetometroP(dataSplit[17])
+                            ConectionS.setMagnetometroY(dataSplit[18])
+            finally:
+                MSP.close()
+                arquivoAltitudeC.write(str(ConectionS.getAltitudeC()))
+                arquivoTemperaturaC.write(str(ConectionS.getTemperaturaC()))
+                arquivoVoltageC.write(str(ConectionS.getVoltageC()))
+                arquivoGpsLatitudeC.write(str(ConectionS.getGpsLatitude()))
+                arquivoGpsLongitudeC.write(str(ConectionS.getGpsLongitudeC()))
+                arquivoGpsAlturaC.write(str(ConectionS.getGpsAlturaC()))
+                arquivoAltitudeP.write(str(ConectionS.getAltitudeP()))
+                arquivoTemperaturaP.write(str(ConectionS.getTemperaturaP()))
+                arquivosetVoltageP.write(str(ConectionS.getVoltageP()))
+                arquivoGiroscopioR.write(str(ConectionS.getGiroscopioR()))
+                arquivoGiroscopioP.write(str(ConectionS.getGiroscopioP()))
+                arquivoGiroscopioY.write(str(ConectionS.getGiroscopioY()))
+                arquivoAcelerometroR.write(str(ConectionS.getAcelerometroR()))
+                arquivoAcelerometroP.write(str(ConectionS.getAcelerometroP()))
+                arquivoAcelerometroY.write(str(ConectionS.getAcelerometroY()))
+                arquivoMagnetometroR.write(str(ConectionS.getMagnetometroR()))
+                arquivoMagnetometroP.write(str(ConectionS.getMagnetometroP()))
+                arquivoMagnetometroY.write(str(ConectionS.getMagnetometroY()))
+                arquivoAltitudeC.close()
+                arquivoTemperaturaC.close()
+                arquivoVoltageC.close()
+                arquivoGpsLongitudeC.close()
+                arquivoGpsAlturaC.close()
+                arquivoAltitudeP.close()
+                arquivoTemperaturaP.close()
+                arquivosetVoltageP.close()
+                arquivoGiroscopioR.close()
+                arquivoGiroscopioP.close()
+                arquivoGiroscopioY.close()
+                arquivoAcelerometroR.close()
+                arquivoAcelerometroP.close()
+                arquivoAcelerometroY.close()
+                arquivoMagnetometroR.close()
+                arquivoMagnetometroP.close()
+                arquivoMagnetometroY.close()
+
         
     # def filterVector(vetor):
     #     cont=0
@@ -135,34 +178,8 @@ class serial:
 #     time.sleep(1)
 #     return 1
     
-# if __name__ == '__main__':
-#     ConectionS()
-#     #a cada 5 segundo o dataset de transição é carregado
-#     cont= 0
-#     tempo= segundo()
-#     cont= tempo + cont
-#     print(cont)
-#     if((cont % 5) == 0):
-#         print('altitude',  ConectionS.altitudeC)
-#         ConectionS.carregaDataset(
-#             ConectionS.altitudeC,
-#             ConectionS.temperaturaC,
-#             ConectionS.voltageC,
-#             ConectionS.gpsLatitudeC,
-#             ConectionS.gpsLongitudeC,
-#             ConectionS.gpsAlturaC,
-#             ConectionS.altitudeP,
-#             ConectionS.temperaturaP,
-#             ConectionS.voltageP,
-#             ConectionS.giroscopioR,
-#             ConectionS.giroscopioP,
-#             ConectionS.giroscopioY,
-#             ConectionS.acelerometroR,
-#             ConectionS.acelerometroP,
-#             ConectionS.acelerometroY,
-#             ConectionS.magnetometroR,
-#             ConectionS.magnetometroP,
-#             ConectionS.magnetometroY
-#             )
-
-        
+if __name__ == '__main__':
+    MSP = serial.Serial('/dev/ttyACM0', 9600, timeout=3)
+    A= serialC()
+    A.Captura(ConectionS())
+    #print(serialC.getAltitudeC())
